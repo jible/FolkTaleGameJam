7b58a33d1c0a82d1cc6b92db58bde329be758ccf
jible <jamezman88th@gmail.com>
2024-06-20 22:30:19 -0700

    raaahhhhh

--- monster/monsterManager.gd
+++ monster/monsterManager.gd
@@ -5,7 +5,7 @@ enum States  { IDLE, PURSUIT, ATTACK1, ATTACK2}
 
 var currentState = States.IDLE
 @onready var facePlayer = self.get_script()
-@export var rotateSpeed = 30
+@export var rotateSpeed = 4
 # @export var dashAcceleration = 100
 # @export var dashSpeed = 500
 
@@ -13,9 +13,10 @@ var currentState = States.IDLE
 @onready var targetPoint = Vector2(0 , 0)
 @export var acceleration = 20
 @onready var speed = 0
-@export var maxSpeed = 200
+@export var maxSpeed = 500
 
 func _physics_process(delta):
+	targetPoint = get_global_mouse_position()
 	updateStateMachine(delta)
 	move_and_slide()
 
@@ -44,6 +45,7 @@ func onExit(state): # End coroutines and animations for one state
 			pass
 		States.PURSUIT:
 			speed = 0
+			velocity = Vector2(0,0)
 		States.ATTACK1:
 			pass
 		States.ATTACK2:
@@ -70,8 +72,10 @@ func idle(delta):
 	return
 
 func pursuit(delta):
-	updateRotate(delta)
+	handleRotation(delta)
 	moveForward(delta)
+	if ( findDistance(position, targetPoint)  < 50):
+		changeState(States.ATTACK1)
 	# If it get close enough to attack, stop pursuing and attack
 	
 	return
@@ -87,49 +91,21 @@ func attack2(delta):
 # --------------------------------------------------------------------------------------------------
 # This code is for making the monster pursue the player/ a point
 
-func updateRotate(delta):
-	# only move if it is not performing another action
-	# This stores the direction the player faces as a vector. we need as an angle in radians
-	var directVector = ( targetPoint - global_position).normalized()
+func handleRotation(delta): # This script makes the monster face the targethttps:
+	# //www.youtube.com/watch?v=tX9yzjigV1k&ab_channel=MasterAlbert
 	# The monster should look in this direction, in order to face its target
-	var targetDirection = directVector.angle()
-	rotateTowards( targetDirection, delta)
-	
-
-func rotateTowards( targetDirection, delta): ## this incrementally rotates the player towards the 
-	# Get the difference between the current angle and the target angle to find which direction it should turn
-	var angleDiff =  targetDirection - rotation
-	if ( angleDiff > 2 * PI):
-		angleDiff -= 2 * PI
-	elif angleDiff < -2 * PI:
-		angleDiff += 2 * PI
-	var rotationDirection = sign(angleDiff)
-	# Rotate in the given direction by direction speed every secon d
-	var rotationAmount = rotateSpeed * rotationDirection * delta
-	
-	# If it gets close, slow down
-	if abs(angleDiff) < PI / 4.0:  # Adjust this threshold as needed
-		rotateSpeed *= 0.9  # Reduce rotation speed as you approach the target direction
-	else:
-		rotateSpeed = 2.0  # Reset or maintain default rotateSpeed
-	
-	rotation+= rotationAmount
-	
-	# If it gets super close, just stop
-	if abs(angleDiff) < 0.01:  
-		rotation = targetDirection 
-		rotateSpeed = 2.0  
-
-
+	var targetAngle = ( targetPoint - $Sprite2D.global_position)
+	var dirOffset = -$Sprite2D.transform.y.angle_to(targetAngle)
+	if (abs(dirOffset) < delta *rotateSpeed):
+		return
+	$Sprite2D.rotate(sign(dirOffset)* min(delta *rotateSpeed, abs(dirOffset)))
 
 func moveForward(delta):
 	speed += acceleration
 	if speed >maxSpeed:
 		speed = maxSpeed
-	velocity = Vector2(0, -1).rotated(rotation) * speed
+	velocity = Vector2(0, -1).rotated($Sprite2D.rotation) * speed
 
 
 func findDistance(pointA, pointB):
-	var xdiff = pointA.x - pointB.x
-	var ydiff = pointA.y - pointB.y
-	return ( sqrt(xdiff*xdiff +ydiff *ydiff))
+	return pointA.distance_to(pointB)
